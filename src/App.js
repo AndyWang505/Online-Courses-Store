645bcad9eb7d4532894050da9d17152b09bdf4cf
andywang505 <andywang890505@gmail.com>
2024-06-03 17:51:24 +0800

    feat: 加入後台頁面新增、刪除、修改功能

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import { useEffect } from 'react';
-import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/admin/Dashboard';
+import AdminProducts from './pages/admin/AdminProducts';
 
 function App() {
-  useEffect(() => {
-    console.log(process.env.REACT_APP_API_URL, process.env.REACT_APP_API_PATH);
-    (async() => {
-      const res = await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/products/all`)
-      console.log(res);
-    })();
-  }, [])
-
   return (
-    <></>
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/admin" element={<Dashboard />}>
+          <Route path='products' element={<AdminProducts />}></Route>
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
